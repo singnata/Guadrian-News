4f5adabab5f9dbf806d8374db971358b61e09e62
singnata <nata.sing7@gmail.com>
2017-10-24 11:27:49 +0300

    second commit

--- js/app.js
+++ js/app.js
@@ -1,39 +1,45 @@
 var app = angular.module('myApp', []);
 
 app.controller('getData', function($scope, $http) {
-	$scope.page = 1;
-    $scope.loadData = function() {
-    	$http.get('http://content.guardianapis.com/search?order-by=newest&show-blocks=body&page=' + $scope.page + '&page-size=10&api-key=7b1d6125-4065-4550-ba22-0547ec51c825')
+  $scope.page = 1;
+  $scope.loadData = function() {
+     $http.get('http://content.guardianapis.com/search?order-by=newest&show-blocks=body&page=' + $scope.page + '&page-size=10&api-key=7b1d6125-4065-4550-ba22-0547ec51c825')
     .then(function(response) {
-        $scope.data = response.data.response;
-        $scope.currentPage = response.data.response.currentPage;
+       $scope.data = response.data.response;
+       $scope.currentPage = response.data.response.currentPage;
     }, function(response) {
-        $scope.content = "Sorry, we couldn't find news for you. Please try again later";
+       $scope.errorMessage = "Sorry, we couldn't find news for you. Please try again later";
+			 }
+		);
+	}
+		
+  $scope.loadData()
+
+	$scope.nextPage = function() {
+    if ($scope.page === $scope.currentPage) {
+      $scope.page ++;
+      $scope.loadData();
     }
-	);
-}
-	$scope.loadData()
+  };
+	
+	$scope.previousPage = function() {
+    if ($scope.currentPage > 1) {
+      $scope.page --;
+      $scope.loadData();
+    }
+  };
 
-$scope.nextPage = function() {
-                if ($scope.page === $scope.currentPage) {
-                    $scope.page ++;
-                    $scope.loadData();
-                }
-            };
-$scope.previousPage = function() {
-                if ($scope.currentPage > 1) {
-                    $scope.page --;
-                    $scope.loadData();
-                }
-            };
-
-
-	});
-
-app.controller('toggle', function($scope) {
-	$scope.hide = true;
-
-})
+	$scope.changePage = function (userValue) {
+		$scope.page = userValue;
+		$scope.loadData();
+		console.log("change",$scope.currentPage);
+		console.log("change",userValue);
+	};
 
 
+  });
+
+	app.controller('accordionController', function($scope) {
+		$scope.hide = true;
+	})
 
