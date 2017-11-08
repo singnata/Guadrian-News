fde6cd1f86f895cd1057e58d8c6f3b043a2ed5fa
singnata <nata.sing7@gmail.com>
2017-11-08 13:59:44 +0200

    converting controllers and services to ES6 Classes

--- js/app.js
+++ js/app.js
@@ -1,93 +1,112 @@
-var app = angular.module('myApp', []);
-
-app.service('newsDataService', function($http) {
-
-    this.loadNewsItems = function(page) {
-      return $http.get('http://content.guardianapis.com/search', {
+class NewsDataService {
+  constructor($http) {
+    this.$http = $http;
+    this.apiKey = '7b1d6125-4065-4550-ba22-0547ec51c825'
+  }
+  loadNewsItems(page) {
+    return this.$http.get('http://content.guardianapis.com/search', {
         params: {
           'order-by': 'newest',
           'page': page,
           'page-size': 10,
-          'api-key': '7b1d6125-4065-4550-ba22-0547ec51c825'
+          'api-key': this.apiKey
         }
       }).then(function(response) {
         return {
           newsItems: response.data.response.results,
-          pages: response.data.response.pages,
-          newsItemId: response.data.response.results.id
+          pages: response.data.response.pages
         };
       })
-    }
-
-    this.loadNewsItemSummary = function(id) {
-      return $http.get('http://content.guardianapis.com/search', {
-        params: {
-          'ids': id,
-          'show-blocks': 'body',
-          'api-key': '7b1d6125-4065-4550-ba22-0547ec51c825'
+  }
+  loadNewsItemSummary(id) {
+    return this.$http.get('http://content.guardianapis.com/search', {
+      params: {
+        'ids': id,
+        'show-blocks': 'body',
+        'api-key': this.apiKey
         }
       }).then(function(response) {
         return {
           newsItemSummary: response.data.response.results[0].blocks.body[0].bodyTextSummary
         };
       })
+  }
+}
+
+class NewsItemController {
+  constructor(NewsDataService) {
+    this.NewsDataService = NewsDataService;
+    this.isLoading = true;
+    this.show = false;
+  }
+  loadNewsItemSummary(id) {
+    this.show = !this.show;
+    if (this.isLoading) {
+      this.NewsDataService.loadNewsItemSummary(id)
+      .then((response) => this.newsItemSummary = response.newsItemSummary)
+      .finally(() => this.isLoading = false) 
     }
-});
+    
+  }
+}
 
-app.controller('NewsItemController', function(newsDataService) {
-  var vm = this;
-  vm.isLoading = true;
-  vm.show = false;
+class NewsFeedController {
+  constructor(NewsDataService) {
+  this.NewsDataService = NewsDataService;
+  this.currentPage = 1;
+  this.errorMessage = false;
+  }
+  loadNewsItems() {
+    this.NewsDataService.loadNewsItems(this.currentPage)
+     .then((response) => {this.newsItems = response.newsItems, this.pages = response.pages}, 
+      (() => this.errorMessage = "Sorry, we couldn't find news for you. Please try again later")); 
+  }
+  changePage(newPage) {
+    this.currentPage = newPage;
+    this.loadNewsItems();   
+  }
+  nextPage() {
+    this.changePage(this.currentPage + 1);
+  }
+  previousPage() {
+    this.changePage(this.currentPage - 1);
+  }
+}
 
-  vm.loadNewsItemSummary = function(id) { 
-    if (vm.isLoading) {
-      newsDataService.loadNewsItemSummary(id).then(function(response) {
-      vm.newsItemSummary = response.newsItemSummary;
-      vm.isLoading = false; 
-      }) 
-    };
-  };
-
-  vm.showNewsTextSummary = function(id) {
-    vm.loadNewsItemSummary(id);
-    vm.show = !vm.show;
-    };
-
-  });
-
-app.controller('NewsFeedController', function(newsDataService) {
-  var vm = this;
-  vm.currentPage = 1;
-
-  vm.loadNewsItems = function() {
-    newsDataService.loadNewsItems(vm.currentPage).then(function(response) {
-      vm.newsItems = response.newsItems;
-      vm.pages = response.pages
-    }, function() {
-      vm.errorMessage = "Sorry, we couldn't find news for you. Please try again later";
-    });
-  };
-
-  vm.loadNewsItems();
-
-  vm.nextPage = function() {
-    vm.changePage(vm.currentPage + 1);
-  };
-
-  vm.previousPage = function() {
-    if (vm.currentPage > 1) {
-      vm.changePage(vm.currentPage - 1);
+class NewsSummaryLimit {
+  constructor($filter) {
+    this.$filter = $filter;
+  }
+  filterMe() {
+    return function($filter, newsSummary, limit) {
+    if (!newsSummary) return;
+    if (newsSummary.length < limit) {
+      return newsSummary;
     }
-  };
-
-  vm.changePage = function(newPage) {
-    vm.currentPage = newPage;
-    vm.loadNewsItems();   
+    return $filter('limitTo')(newsSummary, limit) + '...';
     }
+    
+  }
 
-});
+}
 
- app.filter('newsSummaryLimit', ['$filter', function($filter) {
+angular.module('myApp', []).filter('newsSummaryLimit', ['$filter', function($filter) {
+   return function(newsSummary, limit) {
+    if (!newsSummary) return;
+      if (newsSummary.length < limit) {
+          return newsSummary;
+      }
+      return $filter('limitTo')(newsSummary, limit) + '...';
+   };
+}]);
+
+angular.module('myApp', [])
+    .service('NewsDataService', NewsDataService)
+    .controller('NewsFeedController', NewsFeedController)
+    .controller('NewsItemController', NewsItemController)
+    .filter('NewsSummaryLimit', NewsSummaryLimit);
+
+/* app.filter('newsSummaryLimit', ['$filter', function($filter) {
    return function(newsSummary, limit) {
     if (!newsSummary) return;
       if (newsSummary.length < limit) {
@@ -108,4 +127,4 @@ app.directive('ngEnterBlur', function () {
       }
     });
   };
-});
+});*/
