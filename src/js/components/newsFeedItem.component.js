07c82e24d7baca7acab56b2cddff4d0bad22e999
singnata <nata.sing7@gmail.com>
2017-11-20 12:03:40 +0200

    update components

--- src/js/components/newsFeedItem.component.js
+++ src/js/components/newsFeedItem.component.js
@@ -15,21 +15,21 @@ class NewsFeedItemController {
 };
 const NewsFeedItemComponent = {
   template: `
-      <div class="item">
-        <div>
-          {{$ctrl.item.webTitle}}
-          <span ng-click="$ctrl.loadNewsItemSummary($ctrl.item.id)" class="glyphicon show-content" ng-class="{'glyphicon-menu-right': !$ctrl.show, 'glyphicon-menu-down': $ctrl.show}"></span>
-        </div>
-        <div ng-if="$ctrl.show" class="content">
-          <div class="loader" ng-if="$ctrl.isLoading"></div>
-          {{$ctrl.newsItemSummary | limitFilter : 300}}
-          <a ng-href="{{$ctrl.item.webUrl}}" target="new">Read full news</a>
-        </div>
-      </div>        
+    <div class="item">
+      <div>
+        {{$ctrl.item.webTitle}}
+        <span ng-click="$ctrl.loadNewsItemSummary($ctrl.item.id)" class="glyphicon show-content" ng-class="{'glyphicon-menu-right': !$ctrl.show, 'glyphicon-menu-down': $ctrl.show}"></span>
+      </div>
+      <div ng-if="$ctrl.show" class="content">
+        <div class="loader" ng-if="$ctrl.isLoading"></div>
+        {{$ctrl.newsItemSummary | limitFilter : 300}}
+        <a ng-href="{{$ctrl.item.webUrl}}" target="new">Read full news</a>
+      </div>
+    </div>        
     `,
-    bindings: {
-      item: '<'
-    },
-    controller: NewsFeedItemController
-  };
+  bindings: {
+    item: '<'
+  },
+  controller: NewsFeedItemController
+};
 export default NewsFeedItemComponent;
