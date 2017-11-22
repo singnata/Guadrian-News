91df5964325abd933b721fd383eab8d2ba09c791
singnata <nata.sing7@gmail.com>
2017-11-22 14:17:47 +0200

    removed node_modules

--- node_modules/Guardian-News/js/filters/limitFilter.js
+++
@@ -1,9 +0,0 @@
-angular.module('myApp').filter('limitFilter', function($filter) {
- 	return function(newsSummary, limit) {		    
-  if (!newsSummary) return;		   
-    if (newsSummary.length < limit) {		      
-      return newsSummary;		          
-    }		       
-    return $filter('limitTo')(newsSummary, limit) + '...';		      
-  };		   
-});	
